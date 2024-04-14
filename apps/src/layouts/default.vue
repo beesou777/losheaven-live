1d120768dd1798c064a37b3b78f73db2e0204e93
beesou777 <sbeesou@gmail.com>
2024-04-14 06:27:34 +0545

    feat : added speed anylytics

--- apps/src/layouts/default.vue
+++ apps/src/layouts/default.vue
@@ -5,5 +5,10 @@
       <slot></slot>
     </main>
     <ui-footer />
+    <SpeedInsights />
   </div>
 </template>
+
+<script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/vue';
+</script>
