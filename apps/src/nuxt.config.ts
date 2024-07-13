0d8a578578415af83e0c91b52360d35a1ab157d8
beesou777 <sbeesou@gmail.com>
2024-07-13 09:42:35 +0545

    testing if the text compression works or not

--- apps/src/nuxt.config.ts
+++ apps/src/nuxt.config.ts
@@ -2,7 +2,6 @@ import { createResolver } from '@nuxt/kit';
 const { resolve } = createResolver(import.meta.url);
 
 export default defineNuxtConfig({
-  serverMiddleware: ['compression'],
   devtools: { enabled: false },
   typescript: {
     typeCheck: false,
@@ -82,7 +81,6 @@ export default defineNuxtConfig({
   },
   // eslint-disable-next-line unicorn/expiring-todo-comments
   // TODO: build is consistently failing because of this. check whether we need pre-render check.
-
   routeRules: {
     '/_ipx/**': { headers: { 'cache-control': `public, max-age=31536000, immutable` } },
     '/icons/**': { headers: { 'cache-control': `public, max-age=31536000, immutable` } },
